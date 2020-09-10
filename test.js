f73b70aa23ace332ec640741f38d250ac1fb888f
Aridaniel <aridagnars@gmail.com>
2020-09-10 11:19:50 +0000

    mobile version vinna og exit

--- test.js
+++ test.js
@@ -1,4 +1,18 @@
-console.log("hallo heimur");
+
 document.getElementById("contact-form").addEventListener("submit",(e)=>{
     e.preventDefault()
+})
+
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+var exit = document.getElementById('exit');
+
+menu.addEventListener('click', function(e){
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+})
+
+exit.addEventListener('click', function(e){
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
 })
